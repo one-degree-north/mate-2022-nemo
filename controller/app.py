67232265554a3c42f42fc355e4d1551358488187
Kevin Chen <kevinthekat88@gmail.com>
2022-03-11 11:24:41 +0800

    Controller stuff

---
+++ controller/app.py
@@ -0,0 +1,45 @@
+# import serial
+# ser = serial.Serial('/dev/ttys000')  # open serial port
+# print(ser.name)       # check which port was really used
+# ser.write(b'hello')     # write a string
+# ser.close()
+
+import sys
+import glob
+import serial
+
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+
+ser = serial.Serial('/dev/tty.Bluetooth-Incoming-Port', 9600)  # open serial port
+while True:
+    s = ser.read(10000)
+    print(s)
