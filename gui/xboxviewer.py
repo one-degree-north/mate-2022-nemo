a142fb39180bd774ec4c288bde1a5531e2298afc
Kevin Chen <kevinthekat88@gmail.com>
2022-03-12 18:29:18 +0800

    Update xboxviewer.py
    Complicated stuff is being done

--- gui/xboxviewer.py
+++ gui/xboxviewer.py
@@ -5,53 +5,61 @@ from typing import Union
 
 
 
-class XboxViewer(Canvas):
-
+class XboxViewer():
+    def __init__(self):
+        self.contstate = self.XboxControls()
+    
     class XboxControls():
-        
+
         @dataclass
         class Info():
             """
-            A struct for storing how the controls are displayed in the GUI
+            A struct for storing instructions for displaying the controls in a GUI
+            
             cid -> control id
-            pos -> position
+            
+            relpos -> position
+
             displacement (for 2-axis controls) -> which way the control has been shifted
             in x, y direction
+            
             activation -> For the triggers, any number between 1 and 0. Binary for normal
             buttons
             """
             cid: int
-            pos: tuple[float, float]
-            displacement: tuple[float, float] = None
+            relpos: tuple[float, float]
+
+            radius: float = 10
+            disp: tuple[float, float] = None
             activation: float = 0
-        
-        
-        # Max min height -> 78, 400
-        # Max min width -> 26, 476
 
-        # LTHUMBX = Info(0, ())
-        # LTHUMBY = Info(1)
-        # RTHUMBX = Info(2)
-        # RTHUMBY = Info(3)
-        RTRIGGER = Info(4,)
-        LTRIGGER = Info(5,)
+        def __init__(self):
+            self.controls
 
-        A = Info(6, (368, 195))
-        B = Info(7, (399, 165))
-        X = Info(8, (336, 165))
-        Y = Info(9, (368, 136))
 
-        # B means bumper
-        LB = Info(10, (368, 93))
-        RB = Info(11, (134, 93))
+            self.A = self.Info(6, (0.76, 0.36335))
+            self.B = self.Info(7, (0.82889, 0.27019))
+            self.X = self.Info(8, (0.68889, 0.27019))
+            self.Y = self.Info(9, (0.76, 0.18012))
 
-        BACK = Info(12, (218, 165))
-        START = Info(13, (284, 165))
-        XBOX = Info(14, (251, 111))
+                # B means bumper
+            self.LB = self.Info(10, (0.76, 0.046583))
+            self.RB = self.Info(11, (0.24, 0.46583))
 
-        # LEFTTHUMB = Info(15 )
-        # RIGHTTHUMB = Info(16)
-        DPAD = Info(17, (191, 240), disp=(0, 0))
+            self.BACK = self.Info(12, (0.42667, 0.27019))
+            self.START = self.Info(13, (0.57333, 0.27019))
+            self.XBOX = self.Info(14, (0.5, 0.10248))
+
+                # LEFTTHUMB = self.Info(15 )
+                # RIGHTTHUMB = self.Info(16)
+            self.DPAD = self.Info(17, (0.36667, 0.50311), disp=(0, 0))
+
+    
+
+
+
+viewer = XboxViewer()
+print(viewer.controls.START)
 
     
 
