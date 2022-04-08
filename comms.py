7e43e114f166003100943820438403cbf41cc346
Kevin Chen <kevinthekat88@gmail.com>
2022-04-09 07:18:45 +0800

    Update comms.py

--- comms.py
+++ comms.py
@@ -4,8 +4,8 @@ from serial import *
 
 class Comms:
     def __init__(self, outputQueue=None, controls=None):
-        self.arduinoSerial = Serial(port="/dev/cu.usbmodem14201", baudrate=9600)
-        #"/dev/ttyS0"
+        self.arduinoSerial = Serial(port="/dev/ttyS0", baudrate=9600)
+        # "/dev/ttyS0"
         #"/dev/cu.usbmodem14201"
         #eh make these static
         self.HEADER = bytes.fromhex("AB")
